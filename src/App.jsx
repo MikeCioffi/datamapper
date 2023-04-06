ff8852ca88fdc51ee3f7e1fd46908306ff6e1553
alexgerdisch <alexgerdisch@gmail.com>
2023-04-06 16:07:40 -0700

    Added Logo Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Upload from './components/Upload'
+import Logo from './components/Logo'
 import './App.css'
 
 function App() {
@@ -8,9 +9,12 @@ function App() {
 
   return (
     <div className="App">
+      <Logo />
       <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>      <Upload />
+        Welcome to Dora!
+      </h1>
+      <p>(cause we got the map ya bish)</p>
+      <Upload />
 
     </div>
   )
