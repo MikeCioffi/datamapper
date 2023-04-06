69a18766a9d98f030d2024797c9d3124e3092888
Mike Cioffi <mike.cioffi95@gmail.com>
2023-04-06 18:54:59 -0500

    Create basic theme structure - get tailwind workin

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,42 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "src/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{html,js}',
+    './src/components/**/*.{html,js}',
   ],
   theme: {
-    extend: {},
-  },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#004556',
+      'purple': '#5B1A4D',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  }
+  ,
   plugins: [],
 }
