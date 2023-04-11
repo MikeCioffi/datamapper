b44b296036f0c0dbce7a4b3e4d7fde06a984eee2
Mike Cioffi <mike.cioffi95@gmail.com>
2023-04-10 20:27:27 -0500

    visual updates for large screens

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -50,51 +50,51 @@ function Upload() {
     };
 
     return (
-        <div className="flex flex-col items-center justify-center">
-            <div className="w-full">
-                <div className="flex flex-col justify-center align-middle rounded-lg shadow-md p-6 mb-6 text-center">
-                    <label htmlFor="file-upload" className="relative cursor-pointer">
-                        <span className=" rounded-md shadow-sm py-2 px-3 border border-gray-300">
-                            Upload a file
-                        </span>
-                        <input
-                            id="file-upload"
-                            name="file-upload"
-                            type="file"
-                            accept=".csv"
-                            onChange={changeHandler}
-                            className="sr-only"
-                        />
-                    </label>
+        <div className="flex flex-col items-center justify-center w-full">
 
-                </div>
-                {tableRows.length > 0 && (
-                    <div className="rounded-lg shadow-md p-6">
-                        <div className="flex flex-wrap justify-between -mx-4">
-                            {tableRows.map((rows, index) => (
-                                <div className="w-full sm:w-1/2 lg:w-1/3 px-4 mb-4" key={index}>
-                                    <div className=" rounded-md shadow-md">
-                                        <DraggableBox id={String(index)} rows={rows} index={index} />
-                                    </div>
-                                    <div className="">
-                                        <Dropdown
-                                            id={String(mappingData.findIndex((obj) => obj === rows))}
-                                            Mapping={mappingData}
-                                            setNewMapping={setNewMapping}
-                                            index={index}
-                                        />
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                        <div className="flex justify-end mt-4">
-                            <Export rows={tableRows} mappingData={mappingData} parsedData={parsedData} />
+            <div className="flex flex-col justify-center align-middle rounded-lg shadow-md p-6 mb-6 text-center">
+                <label htmlFor="file-upload" className="relative cursor-pointer">
+                    <span className=" rounded-md shadow-sm py-2 px-3 border border-gray-300">
+                        Upload a file
+                    </span>
+                    <input
+                        id="file-upload"
+                        name="file-upload"
+                        type="file"
+                        accept=".csv"
+                        onChange={changeHandler}
+                        className="sr-only"
+                    />
+                </label>
 
-                        </div>
-                        <Results rows={tableRows} mappingData={mappingData} parsedData={parsedData} />
-                    </div>
-                )}
             </div>
+            {tableRows.length > 0 && (
+                <div className="rounded-lg shadow-md w-full">
+                    <div className="flex flex-wrap justify-between -mx-4">
+                        {tableRows.map((rows, index) => (
+                            <div className="w-full sm:w-1/2 lg:w-1/3 px-4 mb-4" key={index}>
+                                <div className=" rounded-md shadow-md">
+                                    <DraggableBox id={String(index)} rows={rows} index={index} />
+                                </div>
+                                <div className="">
+                                    <Dropdown
+                                        id={String(mappingData.findIndex((obj) => obj === rows))}
+                                        Mapping={mappingData}
+                                        setNewMapping={setNewMapping}
+                                        index={index}
+                                    />
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="flex justify-end mt-4">
+                        <Export rows={tableRows} mappingData={mappingData} parsedData={parsedData} />
+
+                    </div>
+                    <Results rows={tableRows} mappingData={mappingData} parsedData={parsedData} />
+                </div>
+            )}
+
         </div >
     );
 }
